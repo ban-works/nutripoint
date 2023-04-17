10ea4f70d71c8a2855d7d2abaf493406c7a166d9
ban-works <alban.fresil@gmail.com>
2023-04-17 15:08:34 +0200

    DONE upload and sumbit button

--- practice/frontend/src/App.js
+++ practice/frontend/src/App.js
@@ -1,27 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
 import {useState} from 'react';
-const frontendURL = 'http://localhost:3000/'
+const frontendURL = 'http://localhost:3000'
 
 
 function App() {
 
   const [file, setFile] = useState(null)
-
+  console.log('file',file)
 const handleChange= (e)=>{
   setFile(e.target.files);
 
 }
 
 const handleSubmit = ()=>{
-  
+
+  const formData = new FormData();
+  formData.append('fromFront', file[0]);
+console.log(formData);
+  fetch(`${frontendURL}/print_pdf`,{
+    method: 'POST',
+body: formData
+  }).then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      // data.result && dispatch(addPhoto(data.url));
+    });
 }
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <input  type="file" accept="application/pdf" id="icon-button-file"onChange={(e)=>handleChange(e)}/>
+        <div style ={{display:'flex', flexDirection:'column', alignItems:'center', width:'60%'}}>
+
+        <h1> Pdf-printer for Nutripoint</h1>
+        <input  type="file" accept="application/pdf" id="icon-button-file" onChange={(e)=>handleChange(e)}/>
+        <button style={{marginTop:'20px', maxWidth:'200px'}} type="button" onClick={()=> handleSubmit()}> Print selected file</button>
+        </div>
       </header>
     </div>
   );

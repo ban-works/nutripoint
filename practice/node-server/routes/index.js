aede43d1c83b3150a02c9f3a6624bcf76050dbcf
ban-works <alban.fresil@gmail.com>
2023-04-17 13:10:25 +0200

    ADD express-fileupload, unix-print

--- practice/node-server/routes/index.js
+++ practice/node-server/routes/index.js
@@ -1,9 +1,31 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var fs = require("fs");
+var path = require("path");
+var unixPrint = require("unix-print");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res) {
+  res.render("index");
 });
 
+router.post("/print_pdf", async (req, res) => {
+
+  // Access the default printer and save infos
+  const printerInfos = await unixPrint.getDefaultPrinter();
+  console.log(printerInfos);
+
+  // If no printer or printer offline, send an error to frontend
+  if (
+    !printerInfos ||
+    printerInfos.status === undefined ||
+    printerInfos.status.includes("offline") ||
+    printerInfos.alerts.includes("offline")
+  ) {
+    res.json({result:false, error:'The printer is offline'});
+    return;
+  }
+
+
+});
 module.exports = router;
